715bd21f1950a626b7b31f153ed7eda2e2b70872
Heinriches <heydrich.institut@gmail.com>
2025-08-28 21:21:24 -0300

    Final commit for 2.5

--- (book) Desbravando JAVA e Orientacao a Objetos/cap_02/Projeto/livraria/src/livraria/CalculadoraDeEstoque.java
+++ (book) Desbravando JAVA e Orientacao a Objetos/cap_02/Projeto/livraria/src/livraria/CalculadoraDeEstoque.java
@@ -9,5 +9,16 @@ public class CalculadoraDeEstoque {
 		double soma = livroJava8 + livroTDD;
 		
 		System.out.println("O total em estoque é " +soma);
+		
+		if(soma < 150) {
+			System.out.println("Seu estoque está muito baixo!");
+		} else if(soma >= 2000) {
+			System.out.println("Seu estoque está muito alto!");
+		} else {
+			System.out.println("Seu estoque está bom.");
+		}
+	
+	
+	
 	}
 }
