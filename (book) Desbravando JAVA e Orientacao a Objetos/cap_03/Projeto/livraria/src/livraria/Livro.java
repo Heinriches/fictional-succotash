00d0098148331190c91668c6a1019c4c82429bd2
Heinriches <heydrich.institut@gmail.com>
2025-08-29 17:18:15 -0300

    Update Livro.java

--- (book) Desbravando JAVA e Orientacao a Objetos/cap_03/Projeto/livraria/src/livraria/Livro.java
+++ (book) Desbravando JAVA e Orientacao a Objetos/cap_03/Projeto/livraria/src/livraria/Livro.java
@@ -12,9 +12,7 @@ public class Livro {
 	
 	void mostrarDetalhes() {
 		
-		String mensagem = "Mostrando detalhes do livro: ";
-		
-		System.out.println(mensagem);
+		System.out.println("Mostrando detalhes do livro: ");
 		System.out.println("Nome: " +nome);
 		System.out.println("Descrição: " +descricao);
 		System.out.println("Valor: " +valor);
