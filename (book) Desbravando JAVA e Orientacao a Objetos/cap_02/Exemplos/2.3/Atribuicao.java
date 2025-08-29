aec532d129a8bf92b9655e62e12323963718ea6c
Heinriches <heydrich.institut@gmail.com>
2025-08-28 21:00:28 -0300

    Moved project to correct folder

---
+++ (book) Desbravando JAVA e Orientacao a Objetos/cap_02/Exemplos/2.3/Atribuicao.java
@@ -0,0 +1,11 @@
+public class Atribuicao{
+	public static void main(String [] args){
+		int numero = 4;
+		int outroNumeroIgual = numero;
+		
+		numero = numero + 5;
+		
+		System.out.println(numero);
+		System.out.println(outroNumeroIgual);
+	}
+}
