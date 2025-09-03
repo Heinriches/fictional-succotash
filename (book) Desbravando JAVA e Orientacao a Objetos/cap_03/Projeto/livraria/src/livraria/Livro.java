d658614cbdc068efc12d4aad76f74980f420c272
Heinriches <heydrich.institut@gmail.com>
2025-09-03 14:51:56 -0300

    Update Livro.java

--- (book) Desbravando JAVA e Orientacao a Objetos/cap_03/Projeto/livraria/src/livraria/Livro.java
+++ (book) Desbravando JAVA e Orientacao a Objetos/cap_03/Projeto/livraria/src/livraria/Livro.java
@@ -14,15 +14,24 @@ public class Livro {
 	// // 2a
 	void mostrarDetalhes() {
 		
+		// 2a 1
 		System.out.println("Mostrando detalhes do livro: ");
 		System.out.println("Nome: " +nome);
 		System.out.println("Descrição: " +descricao);
 		System.out.println("Valor: " +valor);
 		System.out.println("ISBN: " +isbn);
 		
+		// 2a 2
 		autor.mostrarDetalhes();
 		System.out.println("--");
 				
+	}
+	
+	// // 2b
+	public void aplicaDescontoDe (double percentagem) {
+	
+		// 2b 1
+		this.valor -= this.valor * percentagem;
 	}
 	
 }
