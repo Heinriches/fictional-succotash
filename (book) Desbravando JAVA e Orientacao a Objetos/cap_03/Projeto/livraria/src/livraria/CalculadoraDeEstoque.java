9e446cba4f8949f7a400932daa9b7522f36034ee
Heinriches <heydrich.institut@gmail.com>
2025-09-03 14:22:06 -0300

    Update CalculadoraDeEstoque.java

--- (book) Desbravando JAVA e Orientacao a Objetos/cap_03/Projeto/livraria/src/livraria/CalculadoraDeEstoque.java
+++ (book) Desbravando JAVA e Orientacao a Objetos/cap_03/Projeto/livraria/src/livraria/CalculadoraDeEstoque.java
@@ -3,15 +3,19 @@ package livraria;
 public class CalculadoraDeEstoque {
 	public static void main(String[] args) {
 		
+		// 1
 		double soma = 0;
 		
-		
+		// 2
+		// // 2a
 		for(int i = 0; i< 35; i++) {
 			soma += 59.90;
 		}
 
+		// // 2b
 		System.out.println("O total em estoque é " +soma);
 		
+		// // 2c
 		if(soma < 150) {
 			System.out.println("Seu estoque está muito baixo!");
 		} else if(soma >= 2000) {
@@ -19,8 +23,6 @@ public class CalculadoraDeEstoque {
 		} else {
 			System.out.println("Seu estoque está bom.");
 		}
-	
-	
-	
+		
 	}
 }
