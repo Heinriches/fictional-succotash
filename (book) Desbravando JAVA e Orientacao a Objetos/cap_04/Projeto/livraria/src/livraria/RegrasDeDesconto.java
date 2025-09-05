fd36103bd9cb7015700d2dc66a1f0e9489c2e4b1
Heinriches <heydrich.institut@gmail.com>
2025-09-04 23:15:50 -0300

    Update RegrasDeDesconto.java

--- (book) Desbravando JAVA e Orientacao a Objetos/cap_04/Projeto/livraria/src/livraria/RegrasDeDesconto.java
+++ (book) Desbravando JAVA e Orientacao a Objetos/cap_04/Projeto/livraria/src/livraria/RegrasDeDesconto.java
@@ -17,14 +17,14 @@ public class RegrasDeDesconto {
 	public static void main(String[] args) {
 		
 		Livro livro = new Livro();
-		livro.valor = 59.90;
+		livro.setValor(59.90);
 		
-		System.out.println("Valor atual: " +livro.valor);
+		System.out.println("Valor atual: " +livro.getValor());
 		
 		if(! livro.aplicaDescontoDe(0.1)) {
 			System.out.println("Desconto não pode ser maior do que 30%.");
 		} else {
-			System.out.println("Valor com desconto: " +livro.valor);
+			System.out.println("Valor com desconto: " +livro.getValor());
 		}
 		
 	}
