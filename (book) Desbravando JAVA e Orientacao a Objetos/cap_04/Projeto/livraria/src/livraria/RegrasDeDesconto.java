e71d9741cb3b8dd669f13a042b9dbee4293e5fd2
Heinriches <heydrich.institut@gmail.com>
2025-09-04 21:51:36 -0300

    Update RegrasDeDesconto.java

--- (book) Desbravando JAVA e Orientacao a Objetos/cap_04/Projeto/livraria/src/livraria/RegrasDeDesconto.java
+++ (book) Desbravando JAVA e Orientacao a Objetos/cap_04/Projeto/livraria/src/livraria/RegrasDeDesconto.java
@@ -1,5 +1,13 @@
 package livraria;
 
+/**
+ * A classe RegrasDeDesconto é um ponto-de-entrada para o teste das 
+ * regras-de-negócio relativas à aplicação de desconto sobre os 
+ * itens da livraria.
+ * Ela demonstra a criação e interação com objetos da classe 'Livros'.
+ * 
+ * @author fabio
+ */
 public class RegrasDeDesconto {
 	public static void main(String[] args) {
 		
